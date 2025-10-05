05f35f9fe68e6a6913ddb3dc5120542dab5d4475
rouleauphilippe2026-hub <rouleau.philippe2026@gmail.com>
2025-10-04 20:27:06 -0400

    Create App component with userConfig state

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import SettingsPanel from "./SettingsPanel";
+
+function App() {
+  const [userConfig, setUserConfig] = useState(null);
+
+  return (
+    <div>
+      <SettingsPanel onConfigChange={setUserConfig} />
+      {/* Les modules peuvent utiliser userConfig pour adapter leur comportement */}
+    </div>
+  );
+}
+
+export default App;
